65e6f043068351a994d8a4183f1d68a21bcbc432
Noe Charmet <noe@rolo.chat>
2020-10-09 11:56:08 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# eslint-config-frontend
+# Eslint config frontedn
+
+Eslint config for the frontend teams working on Rolo.
+
+## Install
+
+```bash
+npm i -D @kaneoh/eslint-config-frontend
+```
+
+Add it as a config in `.eslintrc`
+
+```json
+{
+  "extends": ["@kaneoh/eslint-config-frontend"]
+}
+```
---
+++ src/index.js
@@ -0,0 +1,90 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    allowImportExportEverywhere: false,
+    codeFrame: false,
+  },
+  extends: [
+    "google",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react",
+  ],
+  plugins: ["prettier", "import", "react-hooks"],
+  overrides: [
+    {
+      files: ["webpack.*.js"],
+      env: {
+        es6: true,
+        node: true,
+      },
+    },
+  ],
+  ignorePatterns: ["node_modules/", "lib/"],
+  parser: "babel-eslint",
+  globals: {
+    React: "readonly",
+    SparkCommunications: "readonly",
+    process: "readonly",
+    JitsiMeetJS: "readonly",
+  },
+  rules: {
+    indent: "off",
+    "eol-last": "off",
+    "comma-dangle": "off",
+    semi: "off",
+    "key-spacing": "off",
+    "keyword-spacing": "off",
+    "quote-props": "off",
+    "space-before-blocks": "off",
+    "computed-property-spacing": "off",
+    "comma-spacing": "off",
+    "brace-style": "off",
+    "linebreak-style": ["error", "unix"],
+    quotes: "off",
+    "require-jsdoc": "off",
+    "max-len": "off",
+    "operator-linebreak": "off",
+    "valid-jsdoc": "off",
+    "no-console": "error",
+    "prettier/prettier": "error",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "new-cap": "off",
+    "import/order": [
+      "warn",
+      {
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "no-unused-vars": ["error", { args: "after-used" }],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        paths: ["./src"],
+      },
+      "babel-module": {
+        root: "./src",
+        alias: {
+          test: "./test",
+          env: "./env",
+          lib: "./lib",
+        },
+      },
+    },
+  },
+};
